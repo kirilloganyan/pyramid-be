4f64298c2803d8621e7b15cb0c78c2a60c5dea0b
Кирилл Оганян <k.oganyan@gazpromcps.ru>
2024-12-13 07:08:49 +0300

    app fix cors

--- index-server.js
+++ index-server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const userRouter = require('./routes/user.routes')
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
 app.use('/api', userRouter)
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
