84db80707695cf5a315e10073ef447f1746ae70f
Кирилл Оганян <k.oganyan@gazpromcps.ru>
2024-12-17 20:32:59 +0300

    fix cors)

--- index-server.js
+++ index-server.js
@@ -10,27 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: [
-        'http://localhost:3000',
-        'http://91.197.98.228:8080',
-    ],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.options('*', cors());
 
-app.use((req, res, next) => {
-    console.log(`CORS request from origin: ${req.headers.origin}`);
-    next();
-});
-
 app.use('/api', userRouter);
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-});
-
-// Запуск сервера
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
