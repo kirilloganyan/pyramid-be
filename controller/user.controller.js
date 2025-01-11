9e7857b70ac7bf551280549f1c8bc219eba21668
Кирилл Оганян <k.oganyan@gazpromcps.ru>
2025-01-11 20:40:01 +0300

    fix port

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -63,7 +63,7 @@ class UserController {
 const userController = new UserController();
 
 cron.schedule('0 0 * * *', async () => {
-    console.log('Running daily incrementVariable task at 2:05 AM');
+    console.log('Running daily incrementVariable task at 8:41 PM');
     await userController.incrementVariable();
 });
 
